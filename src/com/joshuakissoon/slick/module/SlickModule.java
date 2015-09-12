8cf3a7340c304a8eeb46c3b88dd0e7f17b74c7a6
Joshua Kissoon <JoshuaKissoon@gmail.com>
2015-09-11 20:12:43 -0400

    Added the module class to the slick package

---
+++ src/com/joshuakissoon/slick/module/SlickModule.java
@@ -0,0 +1,17 @@
+package com.joshuakissoon.slick.module;
+
+/**
+ * Interface representation of a module in the slick package
+ *
+ * @author Joshua Kissoon
+ * @since 20150715
+ */
+public interface SlickModule
+{
+
+    /**
+     * Method called to bootstrap the module into the system
+     */
+    public void bootstrap();
+    
+}
